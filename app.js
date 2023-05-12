08ec22a83af8c25a9719156c1795281d683a2175
chandu20web <chandu20.web@gmail.com>
2023-05-12 06:17:26 +0530

    second changes of example

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const isGreaterThan5 = (num) => {
+  console.log(num > 5);
+};
+nums.map((each) => {
+  isGreaterThan5(each);
+});
+
